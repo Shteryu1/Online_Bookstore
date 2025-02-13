d4ce8558a369f689e76cc4d9321342d089438c3e
Shteryu1 <shteryushterev25@itpg-varna.bg>
2025-02-13 18:29:32 +0200

    Add files via upload

---
+++ genres.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 13, 2025 at 05:29 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `online_bookstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genres`
+--
+
+CREATE TABLE `genres` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `genres`
+--
+
+INSERT INTO `genres` (`id`, `name`) VALUES
+(2, 'Дом, градина и хоби'),
+(3, 'Професионална и справочна литература'),
+(4, 'Научна фантастика, фентъзи и хорър'),
+(5, 'Криминални и трилъри'),
+(6, 'Романтични'),
+(7, 'Световна проза'),
+(8, 'Българска литература'),
+(9, 'Поезия и пиеси'),
+(10, 'Хумор'),
+(11, 'Комикси'),
+(12, 'Здраве и семейство'),
+(13, 'Популярна психология'),
+(14, 'История, културология и публицистика'),
+(15, 'Изкуство, фотография, кино, музика'),
+(16, 'Духовност, митология, езотерика'),
+(17, 'Бизнес'),
+(18, 'Наука'),
+(19, 'Туризъм и спорт');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
